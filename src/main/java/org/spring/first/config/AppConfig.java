75b885e1744920c76eb67445f4e6a43aa8d5d727
Maksym Vakuliuk <vakuliukmaksym@gmail.com>
2020-06-03 13:49:41 +0300

    Refactored AppCongig. Properties of db and hibernate moved to db.configuration file

--- src/main/java/org/spring/first/config/AppConfig.java
+++ src/main/java/org/spring/first/config/AppConfig.java
@@ -4,25 +4,32 @@ import java.util.Properties;
 import javax.sql.DataSource;
 import org.apache.commons.dbcp2.BasicDataSource;
 import org.spring.first.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
 
 @Configuration
+@PropertySource("classpath:db.properties")
 @ComponentScan(basePackages = {
         "org.spring.first.dao",
         "org.spring.first.service"
 })
 public class AppConfig {
+    @Autowired
+    private Environment environment;
+
     @Bean
     public DataSource getDataSource() {
         BasicDataSource dataSource = new BasicDataSource();
 
-        dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
-        dataSource.setUrl("jdbc:mysql://localhost/springfirst?serverTimezone=UTC");
-        dataSource.setUsername("MaksymV");
-        dataSource.setPassword("internetShopDBofVM");
+        dataSource.setDriverClassName(environment.getProperty("db.driver"));
+        dataSource.setUrl(environment.getProperty("db.url"));
+        dataSource.setUsername(environment.getProperty("db.username"));
+        dataSource.setPassword(environment.getProperty("db.password"));
         return dataSource;
     }
 
@@ -32,9 +39,9 @@ public class AppConfig {
         sessionFactoryBean.setDataSource(getDataSource());
 
         Properties properties = new Properties();
-        properties.put("hibernate.dialect", "org.hibernate.dialect.MySQL8Dialect");
-        properties.put("hibernate.show.sql", "true");
-        properties.put("hibernate.hbm2ddl.auto", "create-drop");
+        properties.put("hibernate.dialect", environment.getProperty("hibernate.dialect"));
+        properties.put("hibernate.show.sql", environment.getProperty("hibernate.show.sql"));
+        properties.put("hibernate.hbm2ddl.auto", environment.getProperty("hibernate.hbm2ddl.auto"));
         sessionFactoryBean.setHibernateProperties(properties);
         sessionFactoryBean.setAnnotatedClasses(User.class);
         return sessionFactoryBean;
